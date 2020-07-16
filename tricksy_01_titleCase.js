c656620191f0391af1dfd2d93526da3f0b9446ba
Zack Adlington <zadlington@gmail.com>
2020-07-16 15:04:59 +0100

    Completed object tricksy challenges

---
+++ tricksy_01_titleCase.js
@@ -0,0 +1,17 @@
+// Create a function, titleCase, that takes a string and converts it to title case.
+
+let titleCase = (string) => {
+  return string
+    .split(" ")
+    .map((word) => word[0].toUpperCase() + word.slice(1))
+    .join(" ");
+};
+
+console.log(
+  titleCase("hello, my name is Jim") // "Hello, My Name Is Jim"
+);
+
+// Zack's tests
+console.log(
+  titleCase("testing the poo emoji 💩") // "Testing The Poo Emoji 💩 "
+);
---
+++ tricksy_02_frequency.js
@@ -0,0 +1,43 @@
+// Create a function called frequency that takes a string as an argument. It should return an object where each key represents a letter that appears in the string and the value is how many times that letter appears. It doesn't need to count spaces and it should ignore whether the letter is uppercase or lowercase.
+
+let frequency = (string) => {
+  return string
+    .toLowerCase()
+    .split("")
+    .filter((char) => char !== " ")
+    .reduce((counts, char) => {
+      char in counts ? counts[char]++ : (counts[char] = 1);
+      return counts;
+    }, {});
+};
+
+console.log(frequency("The quick brown fox jumps over the lazy dog"));
+
+/*
+    {
+        a: 1, b: 1, c: 1, d: 1, e: 3, f: 1, g: 1, h: 2, i: 1, j: 1, k: 1, l: 1, m: 1,
+        n: 1, o: 4, p: 1, q: 1, r: 2, s: 1, t: 2, u: 2, v: 1, w: 1, x: 1, y: 1, z: 1
+    }
+*/
+
+console.log(frequency("Wkh sdvvzrug lv vhyhq grqw whoo dqbrqh"));
+
+/*
+    {
+        b: 1, d: 2, g: 2, h: 5, k: 1, l: 1, o: 2,
+        q: 4, r: 3, s: 1, u: 1, v: 4, w: 3, y: 1, z: 1
+    }
+*/
+
+// `forEach` solution
+// let frequency = (string) => {
+//     let counts = {};
+//     string
+//       .toLowerCase()
+//       .split("")
+//       .filter((char) => char !== " ")
+//       .forEach((char) => {
+//         counts[char] ? (counts[char] += 1) : (counts[char] = 1);
+//       });
+//     return counts;
+//   };
