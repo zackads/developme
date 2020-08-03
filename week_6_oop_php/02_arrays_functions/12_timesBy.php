1e9c6d48794be26bf6c32c8407cafe16f18995c6
Zack Adlington <zadlington@gmail.com>
2020-08-03 15:51:53 +0100

    Complete timesBy()

---
+++ week_6_oop_php/02_arrays_functions/12_timesBy.php
@@ -0,0 +1,14 @@
+<?php
+
+require __DIR__ . "/vendor/autoload.php";
+
+function timesBy($arr, $num) {
+    return collect($arr)->map(fn($val) => $val * $num)->all();
+}
+
+dump(
+    timesBy([2, 3, 4, 5, 6], 10), // [20, 30, 40, 50, 60]
+    timesBy([2, 3, 4], 5), // [10, 15, 20]
+    timesBy([2, 3, 4, 5, 6, 7], -5), // [-10, -15, -20, -25, -30, -35]
+    timesBy([-3, -4, -5, -6], -5), // [15, 20, 25, 30]
+);
