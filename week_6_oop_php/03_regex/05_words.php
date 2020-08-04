16895bf629addd0e1d7a8ab75b84eddeab7e297a
Zack Adlington <zadlington@gmail.com>
2020-08-04 12:39:31 +0100

    Complete 05_words.php

---
+++ week_6_oop_php/03_regex/05_words.php
@@ -0,0 +1,10 @@
+<?php
+
+require __DIR__ . "/vendor/autoload.php";
+
+function words($str) {
+    return preg_split("/\s/", $str);
+}
+
+dump(words("This is a sentence")); // ["This", "is", "a", "sentence"]
+dump(words("Hello, my name is Dolemite")); // ["Hello", "my", "name", "is", "Dolemite"]
