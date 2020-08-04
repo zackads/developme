0f6c3ef4e9d0c7aed705db05030a363e1ae5040c
Zack Adlington <zadlington@gmail.com>
2020-08-04 16:03:45 +0100

    Complete 05_stringy.php

---
+++ week_6_oop_php/04_classes/05_stringy.php
@@ -0,0 +1,42 @@
+<?php
+
+require __DIR__ . "/vendor/autoload.php";
+
+class Stringy
+{
+    private $string;
+
+    public function __construct($string) {
+        $this->string = $string;
+    }
+
+    public function lower() {
+        return strtolower($this->string);
+    }
+
+    public function upper() {
+        return strtoupper($this->string);
+    }
+
+    public function append($string) {
+        return $this->string . $string;
+    }
+
+    public function repeat($num) {
+        return str_repeat($this->string, $num);
+    }
+}
+
+$string = new Stringy("Na");
+
+// it can lowercase a string
+dump($string->lower()); // "na"
+
+// it can uppercase a string
+dump($string->upper()); // "NA"
+
+// it can concatenate
+dump($string->append("blah")); // "Nablah"
+
+// it can repeat a string
+dump($string->repeat(5)); // "NaNaNaNaNa"
