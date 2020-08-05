b8bda7b2aade01a0e4e35b2b0f23c52846c57ae7
Zack Adlington <zadlington@gmail.com>
2020-08-05 11:34:13 +0100

    Add chaining to setter methods from 04_classes exercses

--- week_6_oop_php/04_classes/02_lightSwitch.php
+++ week_6_oop_php/04_classes/02_lightSwitch.php
@@ -14,11 +14,13 @@ class Lightswitch
     public function turnOn()
     {
         $this->on = true;
+        return $this;
     }
 
     public function turnOff()
     {
         $this->on = false;
+        return $this;
     }
 }
 
@@ -30,6 +32,9 @@ dump($lightSwitch->isOn()); // false
 // you can turn it on
 $lightSwitch->turnOn();
 dump($lightSwitch->isOn()); // true
+
+// you can turn it on and off again
+dump($lightSwitch->turnOn()->turnOff()->isOn()); // false
 
 // you can turn it off
 $lightSwitch->turnOff();
--- week_6_oop_php/04_classes/04_address.php
+++ week_6_oop_php/04_classes/04_address.php
@@ -18,16 +18,19 @@ class Address
     public function setStreet($street)
     {
         $this->street = $street;
+        return $this;
     }
 
     public function setPostcode($postcode)
     {
         $this->postcode = $postcode;
+        return $this;
     }
 
     public function setTown($town)
     {
         $this->town = $town;
+        return $this;
     }
 
     public function fullAddress()
@@ -41,7 +44,6 @@ class Address
             $this->town,
             $this->postcode,
         ]);
-
     }
 
 }
@@ -55,6 +57,9 @@ dump($address->fullAddress()); // "1 Made Up Street, Bristol, BS4 8TR"
 $address->setStreet("12 Cantelope Way");
 $address->setPostcode("SW5 8RQ");
 $address->setTown("Swansea");
+
+// or update the street, postcode and town using method chaining
+$address->setStreet("42 Walaby Way")->setPostcode("F1ND N3M0")->setTown("Sydney");
 
 // logs the new full address neatly
 dump($address->fullAddress()); // "12 Cantelope Way, Swansea, SW5 8RQ"
