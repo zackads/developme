3388260a3ddaf4961a6a37bc3856bf62aad8ef1c
Zack Adlington <zadlington@gmail.com>
2020-08-25 14:10:25 +0100

    Turn header from functional to class-based component

--- week_9_react/hello-world/src/Header.js
+++ week_9_react/hello-world/src/Header.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Header = ({ children }) => (
-  <header className="jumbotron">
-    <h1>{children}</h1>
-    <p>It's actually amazing</p>
-  </header>
-);
+class Header extends Component {
+  render() {
+    const { children } = this.props;
+
+    return (
+      <header className="jumbotron">
+        <h1>{children}</h1>
+        <p>It's actually amazing</p>
+      </header>
+    );
+  }
+}
 
 export default Header;
