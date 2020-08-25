c93405062f7a9867a66167e20b755172ec72d411
Zack Adlington <zadlington@gmail.com>
2020-08-25 15:25:19 +0100

    Create ToggleText component

--- week_9_react/hello-world/src/App.js
+++ week_9_react/hello-world/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Stuff from "./Stuff.js";
 import Clicked from "./Clicked.js";
+import ToggleText from "./ToggleText.js";
 
 const App = () => (
   <React.Fragment>
     <Stuff square={true} color="purple" />
     <Clicked />
+    <ToggleText initial="Hello" alternate="World" />
   </React.Fragment>
 );
 
---
+++ week_9_react/hello-world/src/ToggleText.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+class ToggleText extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      initial: true,
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({
+      initial: !this.state.initial,
+    });
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <p>{this.state.initial ? this.props.initial : this.props.alternate}</p>
+        <button class="btn btn-danger" onClick={this.handleClick}>
+          Toggle
+        </button>
+      </React.Fragment>
+    );
+  }
+}
+
+export default ToggleText;
