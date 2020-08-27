1709baef5c8604b5ef62ee8d7e9fe0f97e686712
Zack Adlington <zadlington@gmail.com>
2020-08-27 15:26:26 +0100

    Add 404 component and routing

--- week_9_react/day_3/src/App.js
+++ week_9_react/day_3/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import "./App.css";
 import Length from "./Length.js";
@@ -9,32 +9,36 @@ import TempConverter from "./TempConverter.js";
 import List from "./List.js";
 import Adder from "./Adder.js";
 import Transform from "./Transform.js";
+import FourOhFour from "./FourOhFour.js";
 import Footer from "./Footer.js";
 
 function App() {
   return (
     <Router>
       <>
-        <Route exact path="/length">
-          <Length />
-        </Route>
-        <Route exact path="/passwordstrength">
-          <PasswordStrength />
-        </Route>
-        <Route exact path="/tempconverter">
-          <TempConverter />
-        </Route>
-        <Route exact path="/list">
-          <List />
-        </Route>
-        <Route exact path="/adder">
-          <Adder />
-        </Route>
-        <Route exact path="/transform">
-          <Transform function={(x) => Math.pow(x, 2)} />
-          <Transform function={(x) => Math.pow(x, 7)} />
-          <Transform function={(x) => x + x} />
-        </Route>
+        <Switch>
+          <Route exact path="/length">
+            <Length />
+          </Route>
+          <Route exact path="/passwordstrength">
+            <PasswordStrength />
+          </Route>
+          <Route exact path="/tempconverter">
+            <TempConverter />
+          </Route>
+          <Route exact path="/list">
+            <List />
+          </Route>
+          <Route exact path="/adder">
+            <Adder />
+          </Route>
+          <Route exact path="/transform">
+            <Transform function={(x) => Math.pow(x, 2)} />
+            <Transform function={(x) => Math.pow(x, 7)} />
+            <Transform function={(x) => x + x} />
+          </Route>
+          <FourOhFour />
+        </Switch>
         <Footer />
       </>
     </Router>
