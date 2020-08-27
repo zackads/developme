768ff41cb33eb6ba5f42716f83da92046cead46a
Zack Adlington <zadlington@gmail.com>
2020-08-27 15:17:01 +0100

    Create routes for components made yesterday

--- week_9_react/day_3/src/App.js
+++ week_9_react/day_3/src/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 import "./App.css";
 import Length from "./Length.js";
 import PasswordStrength from "./PasswordStrength.js";
@@ -6,14 +9,34 @@ import TempConverter from "./TempConverter.js";
 import List from "./List.js";
 import Adder from "./Adder.js";
 import Transform from "./Transform.js";
+import Footer from "./Footer.js";
 
 function App() {
   return (
-    <>
-      <Transform function={(x) => Math.pow(x, 2)} />
-      <Transform function={(x) => Math.pow(x, 7)} />
-      <Transform function={(x) => x + x} />
-    </>
+    <Router>
+      <>
+        <Route exact path="/length">
+          <Length />
+        </Route>
+        <Route exact path="/passwordstrength">
+          <PasswordStrength />
+        </Route>
+        <Route exact path="/tempconverter">
+          <TempConverter />
+        </Route>
+        <Route exact path="/list">
+          <List />
+        </Route>
+        <Route exact path="/adder">
+          <Adder />
+        </Route>
+        <Route exact path="/transform">
+          <Transform function={(x) => Math.pow(x, 2)} />
+          <Transform function={(x) => Math.pow(x, 7)} />
+          <Transform function={(x) => x + x} />
+        </Route>
+      </>
+    </Router>
   );
 }
 
